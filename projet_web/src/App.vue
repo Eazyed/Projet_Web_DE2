b46974d4f35979f01ae58abc9fedf92c581ee5ad
Mathias RUELLE <mathias.ruelle@etu.univ-st-etienne.fr>
2019-09-13 09:49:21 +0200

    Le commit

--- projet_web/src/App.vue
+++ projet_web/src/App.vue
@@ -1,16 +1,75 @@
 <template>
   <div id="app">
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="warning">
+        <b-navbar variant="faded" type="light">
+          <b-navbar-brand href="/" class="d-inline-block align-top">
+            <img id="logo" src="./assets/logobiereandco.png" alt="Logo" />
+            BiereAndCo
+          </b-navbar-brand>
+        </b-navbar>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/authent">Link</b-nav-item>
+            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <!-- <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+            </b-nav-form>-->
+
+            <!-- <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown>-->
+
+            <b-nav-item-dropdown v-if="isAuthentified" right>
+              <!-- Using 'button-content' slot -->
+
+              <template v-slot:button-content>
+                <em>Logged In</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <!-- Using 'button-content' slot -->
+            <b-nav-item v-else right to="/authent">Log In</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+    <router-view />
+  </div>
+  <!-- <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> | 
+      <router-link to="/authent">Authentification</router-link>
     </div>
     <router-view/>
-  </div>
+  </div>-->
 </template>
 
+<script lang="ts">
+import Vue from "vue";
+export default class App extends Vue {
+  public get isAuthentified() {
+    return false;
+  }
+}
+</script>
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -27,5 +86,8 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+#logo {
+  height: 50px;
 }
 </style>
--- projet_web/src/views/Home.vue
+++ projet_web/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logobiereandco.png">
     <template>
   <div id="app" class="container">
     <br />
