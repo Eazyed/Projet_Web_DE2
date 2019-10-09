ad1c188c8f5d2e814d75d9d56ecbf755e1182aac
Mathias RUELLE <mathias.ruelle@etu.univ-st-etienne.fr>
2019-10-09 14:44:13 +0200

    Ajout vue produit

--- projet_web/src/router.ts
+++ projet_web/src/router.ts
@@ -30,12 +30,20 @@ export default new Router({
       component : Authentification,
     },
     {
+      path: '/product/:productname?',
+      name: 'product',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/ProductView.vue'),
+    },
+    {
       path: '/catalog',
       name: 'catalog',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component : Catalog,
-    }
+    },
   ],
 });
---
+++ projet_web/src/views/ProductView.vue
@@ -0,0 +1,35 @@
+<template>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Product } from "../models/Product";
+import json from "../assets/donnees.json";
+import { ObjectMapper } from 'json-object-mapper';
+
+
+@Component({
+  components: {
+  }
+})
+export default class ProductView extends Vue {
+  private fullCatalog: Product[] = new Array<Product>();
+  private itemToDisplay: Product = new Product();
+
+  mounted() {
+    this.GetCatalog();
+    this.fullCatalog.forEach(x => {
+        if(x.name===this.$route.params.productname){
+            this.itemToDisplay=x;
+        }
+    })
+    //this.$route.params.productname
+  }
+  private GetCatalog(){      
+      this.fullCatalog=ObjectMapper.deserializeArray(Product, json);
+  }
+}
+</script>
+
+<style scoped>
+</style>
