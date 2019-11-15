ce7c083aa617a9c94f94e15bc990e350949336ab
Mathias RUELLE <mathias.ruelle@etu.univ-st-etienne.fr>
2019-11-15 12:40:26 +0100

    NSM

--- projet_web/src/App.vue
+++ projet_web/src/App.vue
@@ -14,6 +14,7 @@
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
             <b-nav-item to="/catalog">Catalogue</b-nav-item>
+            <b-nav-item to="/cart">Panier</b-nav-item>
           </b-navbar-nav>
 
           <!-- Right aligned nav items -->
